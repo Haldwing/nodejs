0d0c02538200eb671dd6e4d110235e0b7f540a09
Haldwing <139230101+Haldwing@users.noreply.github.com>
2024-03-10 14:22:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import bcrypt from "bcrypt";
-import passport from "passport";
-import { Strategy } from "passport-local";
-import GoogleStrategy from "passport-google-oauth2";
-import session from "express-session";
+
 
 
 const app = express();
 const port = 3000;
-const saltRounds = 10;
 
 
-app.use(
-    session({
-      secret: "marjorie",
-      resave: false,
-      saveUninitialized: true,
-      cookie: {
-        maxAge: 1000 * 60 * 60
-      }
-    })
-);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(passport.initialize());
-app.use(passport.session());  
+
 
 const db = new pg.Client({
     user: "postgres",
@@ -67,7 +51,6 @@ app.get("/", (req, res) => {
 
 
 app.get("/home", async (req, res) => {
-    if (req.isAuthenticated()){
     try { 
         const result = await db.query("SELECT * FROM bookinfo ORDER BY id ASC");
         res.render("index.ejs", {
@@ -77,9 +60,7 @@ app.get("/home", async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-} else {
-    res.redirect("/login");
-}
+
 });
 
 app.get("/compose", async (req, res) => {
@@ -87,7 +68,7 @@ app.get("/compose", async (req, res) => {
 });
 
 app.get("/pages/:id", async (req, res) => {
-    if (req.isAuthenticated()){
+ 
     const id = req.params.id;
     const post = await db.query(`SELECT * FROM bookinfo WHERE id = ${id}`);
     const notes = await db.query(`SELECT * FROM booknotes WHERE book_id = ${id}`);
@@ -95,72 +76,26 @@ app.get("/pages/:id", async (req, res) => {
             book: post.rows[0],
             notes: notes.rows
         });
-    } else {
-        res.redirect("/login");
-    }
+  
 
 });
 
 
 
 app.get("/edit/:id", async (req, res) => {
-    if (req.isAuthenticated()){
+   
     const id = req.params.id;
     const post = await db.query(`SELECT * FROM bookinfo WHERE id = ${id}`);
     res.render("edit.ejs", {
         book: post.rows[0]
     });
-    } else {
-        res.redirect("/login");
-    }
 });
 
-app.get("/logout", (req, res) => {
-    req.logout(function (err) {
-      if (err) {
-        return next(err);
-      }
-      res.redirect("/");
-    });
-  });
 
-app.post(
-    "/login",
-    passport.authenticate("local", {
-      successRedirect: "/home",
-      failureRedirect: "/login",
-    })
-);
 
-app.post("/register", async (req, res) => {
-    const email = req.body.username;
-    const password = req.body.password;
 
-    try {
-        const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [email]);
 
-        if (checkResult.rows.length > 0) {
-            res.redirect("/login");
-        } else {
-            bcrypt.hash(password, saltRounds, async (err, hash) => {
-                if (err) {
-                    console.error("Error hashing password:", err);
-                } else {
-                    const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *", 
-                    [email, hash]
-                );
-                const user = result.rows[0];
-                req.login(user, (err) => {
-                    console.log("success");
-                    res.redirect("/home");
-                });
-            }
-        });
-        }
-    } catch (err) {
-        console.log(err);
-    }
-});
+
 
 app.post("/compose", async (req, res) => {
     const { title, isbn, author, description, rating } = req.body;
@@ -174,83 +109,40 @@ app.post("/compose", async (req, res) => {
 });
 
 app.post("/edit/:id", async (req, res) => {
-    if (req.isAuthenticated()){
+    
     const id = req.params.id;
     const { title, isbn, author, description, rating } = req.body;
     await db.query(`UPDATE bookinfo SET title = '${title}', isbn = ${isbn}, author = '${author}', description = '${description}', rating = ${rating} WHERE id = ${id}`);
     res.redirect("/");
-    } else {
-        res.redirect("login");
-    }
 });
 
 
 
 app.post("/editnotes/:id", async (req, res) => {
-    if (req.isAuthenticated()){
+   
     const id = req.params.id;
     const notes = req.body.mynotes;
     await db.query(`INSERT INTO booknotes (notes, book_id) VALUES ('${notes}', ${id})`);
     res.redirect(`/pages/${id}`);
-    } else {
-        res.redirect("/login");
-    }
+
 });
 
 
 app.post("/sort", async (req, res) => {
-    if (req.isAuthenticated()){
+    
     const sort = req.body.sort;
-    const result = await db.query(`SELECT * FROM booknotes ORDER BY ${sort} DESC`);
+    const result = await db.query(`SELECT * FROM bookinfo ORDER BY ${sort} DESC`);
     res.render("index.ejs", {
         books: result.rows
     });
-    } else {
-        res.redirect("/");
-    }
+  
 });
 
 
 
-passport.use(
-    "local",
-    new Strategy(async function verify(username, password, cb) {
-        try {
-            const result = await db.query("SELECT * FROM users WHERE email = $1", [
-                username,
-            ]);
-            if (result.rows.length > 0) {
-                const user = result.rows[0];
-                const storedHashedPassword = user.password;
-                bcrypt.compare(password, storedHashedPassword, (err, valid) => {
-                    if (err) {
-                        console.error("Error comparing passwords:", err);
-                        return cb(err);
-                    } else {
-                        if (valid) {
-                            return cb(null, user);
-                        } else {
-                            return cb(null, false);
-                        }
-                    }
-                });
-            } else {
-                return cb("User not found");
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    })
-);
 
 
-passport.serializeUser((user, cb) => {
-    cb(null, user);
-});
 
-passport.deserializeUser((user, cb) => {
-    cb(null, user);
-});
 
 
 app.listen(port, () => {
